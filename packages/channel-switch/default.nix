af0fb27e7411a246af4498685bff1df9921d3023
roopa <roopa.shanmugam@unikie.com>
2024-12-19 08:28:44 +0400

    Added python packages in requirements

--- packages/channel-switch/default.nix
+++ packages/channel-switch/default.nix
@@ -1,19 +1,20 @@
 { config, pkgs, lib, ... }:
-  let
-    pyproject = lib.importTOML (config.mkDerivation.src + /pyproject.toml);
-  in
+
+let
+  pyproject = lib.importTOML (config.mkDerivation.src + "/pyproject.toml");
+in
+
 {
   imports = [
     dream2nix.modules.dream2nix.pip
   ];
 
-  deps =
-    { nixpkgs, ... }:
-    {
-      python = nixpkgs.python3;
-    };
+  deps = { nixpkgs, ... }: {
+    python = nixpkgs.python3;
+  };
 
   inherit (pyproject.project) name version;
+
   buildPythonPackage = {
     pyproject = lib.mkForce true;
     build-system = [ config.deps.python.pkgs.setuptools ];
@@ -30,8 +31,7 @@
     pipFlags = [ "--no-deps" ];
     nativeBuildInputs = [ config.deps.gcc ];
   };
-}
-  with lib; {
+
   config = {
     # Systemd service definition
     systemd.services.channel-switch = {
@@ -40,23 +40,23 @@
       serviceConfig = {
         Environment = "PATH=/run/current-system/sw/bin:/run/wrappers/bin:/bin:/usr/bin";
         ExecStart = "/run/current-system/sw/bin/channel-switch";
-        Restart = "no"; #"on-failure";
+        Restart = "no"; # "on-failure";
         RestartSec = "5s";
+        User = "root";
       };
-      serviceConfig.User = "root";
       after = [
         "mdmagent.service"
-      ];  # Ensure these services start first
+      ];
       wants = [
         "mdmagent.service"
-      ]; 
+      ];
     };
 
     # Ensure the required packages are installed
     environment.systemPackages = [
       pkgs.python3
       pkgs.wpa_supplicant
-      (pkgs.python4Packages.buildPythonApplication {
+      (pkgs.python3Packages.buildPythonApplication {
         pname = "channel-switch";
         version = "1.0.0";
         src = ./.;
@@ -70,6 +70,7 @@
           pkgs.python3Packages.systemd
           pkgs.python3Packages.msgpack
         ];
+
         meta = with lib; {
           description = "Resilient Mesh Automatic Channel Selection";
           license = licenses.asl20;
