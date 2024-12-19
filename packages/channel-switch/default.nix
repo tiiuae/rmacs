15322cce0b391cccc84da7ca43d787ccd688b296
roopa <roopa.shanmugam@unikie.com>
2024-12-19 07:48:07 +0400

    Added python packages in requirements

--- packages/channel-switch/default.nix
+++ packages/channel-switch/default.nix
@@ -1,17 +1,15 @@
-{ config, pkgs, lib, dream2nix, ... }: 
-let
-  pyproject = lib.importTOML (config.mkDerivation.src + /pyproject.toml);
+{ config, pkgs, lib, dream2nix, ... }: with lib; let
+  pyproject = lib.importTOML (./pyproject.toml);
   pkgsCross = import <nixpkgs> { system = config.system; };
 
-in
-{
+in {
   imports = [
     dream2nix.modules.dream2nix.pip
   ];
 
-  deps =
-    { nixpkgs, ... }:
-    {
+  config = {
+    # Dependencies definition
+    deps = { nixpkgs, ... }: {
       python = nixpkgs.python3;
       inherit (nixpkgs)
         batctl
@@ -31,72 +29,9 @@ in
         gawk
         customWpaSupplicant
         hostapd
-        radvd
-        ;
+        radvd;
     };
 
-  inherit (pyproject.project) name version;
-
-  mkDerivation = {
-    src = lib.cleanSourceWith {
-      src = lib.cleanSource ./.;
-      filter =
-        name: type:
-          !(builtins.any (x: x) [
-            (lib.hasSuffix ".nix" name)
-            (lib.hasPrefix "." (builtins.baseNameOf name))
-            (lib.hasSuffix "flake.lock" name)
-          ]);
-    };
-    buildInputs = [ config.deps.bash ];
-    propagatedBuildInputs = [ config.deps.batctl ];
-    postFixup =
-      let
-        binPath = lib.makeBinPath (
-          with config.deps;
-          [
-            ebtables
-            openssl
-            libfaketime
-            batctl
-            killall
-            iw
-            iproute2
-            kmod
-            coreutils
-            gnugrep
-            gawk
-            gnused
-          ]
-        );
-      in
-      ''
-        ${builtins.foldl' (s: p: s + "wrapProgram $out/bin/${p} --set PATH ${binPath};") "" [
-          "config_converter_mesh.sh"
-        ]}
-      '';
-  };
-
-  buildPythonPackage = {
-    pyproject = lib.mkForce true;
-    build-system = [ config.deps.python.pkgs.setuptools ];
-    pythonImportsCheck = [
-      "channel-switch"
-    ];
-  };
-
-  pip = {
-    editables.${pyproject.project.name} = "./channel-switch";
-    requirementsList = pyproject.project.dependencies or [ ];
-    requirementsFiles = pyproject.tool.setuptools.dynamic.dependencies.file or [ ];
-    flattenDependencies = true;
-    pipFlags = [ "--no-deps" ];
-    nativeBuildInputs = [ config.deps.gcc ];
-  };
-}
-
-with lib; {
-  config = {
     # Systemd service definition
     systemd.services.channel-switch = {
       description = "Resilient Mesh Automatic Channel Selection";
@@ -104,16 +39,12 @@ with lib; {
       serviceConfig = {
         Environment = "PATH=/run/current-system/sw/bin:/run/wrappers/bin:/bin:/usr/bin";
         ExecStart = "/run/current-system/sw/bin/channel-switch";
-        Restart = "no"; #"on-failure";
+        Restart = "no";  # "on-failure" can be set if needed
         RestartSec = "5s";
+        User = "root";
       };
-      serviceConfig.User = "root";
-      after = [
-        "mdmagent.service"
-      ];  # Ensure these services start first
-      wants = [
-        "mdmagent.service"
-      ]; 
+      after = [ "mdmagent.service" ];
+      wants = [ "mdmagent.service" ];
     };
 
     # Ensure the required packages are installed
@@ -125,27 +56,67 @@ with lib; {
         version = "1.0.0";
         src = ./.;
 
-        build-system = with pkgs.python3Packages; [
-          setuptools
-        ];
+        build-system = with pkgs.python3Packages; [ setuptools ];
 
         propagatedBuildInputs = [
           pkgs.python3Packages.pyyaml
           pkgs.python3Packages.systemd
           pkgs.python3Packages.msgpack
         ];
+
         meta = with lib; {
           description = "Resilient Mesh Automatic Channel Selection";
           license = licenses.asl20;
           maintainers = [
-            {
-              name = "Roopa Shanmugam";
-              email = "roopa.shanmugam@tii.ae";
-            }
+            { name = "Roopa Shanmugam"; email = "roopa.shanmugam@tii.ae"; }
           ];
         };
       })
     ];
+
+    # Build derivation settings
+    mkDerivation = {
+      src = lib.cleanSourceWith {
+        src = lib.cleanSource ./.;
+        filter = name: type:
+          !(builtins.any (x: x) [
+            (lib.hasSuffix ".nix" name)
+            (lib.hasPrefix "." (builtins.baseNameOf name))
+            (lib.hasSuffix "flake.lock" name)
+          ]);
+      };
+      buildInputs = [ config.deps.bash ];
+      propagatedBuildInputs = [ config.deps.batctl ];
+      postFixup = let
+        binPath = lib.makeBinPath (
+          with config.deps; [
+            ebtables openssl libfaketime batctl killall iw iproute2 kmod
+            coreutils gnugrep gawk gnused
+          ]
+        );
+      in
+      ''
+        ${builtins.foldl' (s: p: s + "wrapProgram $out/bin/${p} --set PATH ${binPath};") "" [
+          "config_converter_mesh.sh"
+        ]}
+      '';
+    };
+
+    # Python package build settings
+    buildPythonPackage = {
+      pyproject = lib.mkForce true;
+      build-system = [ config.deps.python.pkgs.setuptools ];
+      pythonImportsCheck = [ "channel-switch" ];
+    };
+
+    # Pip configuration
+    pip = {
+      editables.${pyproject.project.name} = "./channel-switch";
+      requirementsList = pyproject.project.dependencies or [ ];
+      requirementsFiles = pyproject.tool.setuptools.dynamic.dependencies.file or [ ];
+      flattenDependencies = true;
+      pipFlags = [ "--no-deps" ];
+      nativeBuildInputs = [ config.deps.gcc ];
+    };
   };
 }
-
