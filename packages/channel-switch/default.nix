24237635964621fc513763ab2beefcd97a830962
roopa <roopa.shanmugam@unikie.com>
2024-12-19 08:10:35 +0400

    Added python packages in requirements

--- packages/channel-switch/default.nix
+++ packages/channel-switch/default.nix
@@ -1,13 +1,5 @@
-{ config, pkgs, lib, dream2nix, ... }: with lib; let
-  pyproject = lib.importTOML (./pyproject.toml);
-  pkgsCross = import <nixpkgs> { system = config.system; };
-
-in {
-  imports = [
-    dream2nix.modules.dream2nix.pip
-  ];
-
-
+{ config, pkgs, lib, ... }: with lib; {
+  config = {
     # Systemd service definition
     systemd.services.channel-switch = {
       description = "Resilient Mesh Automatic Channel Selection";
@@ -15,42 +7,47 @@ in {
       serviceConfig = {
         Environment = "PATH=/run/current-system/sw/bin:/run/wrappers/bin:/bin:/usr/bin";
         ExecStart = "/run/current-system/sw/bin/channel-switch";
-        Restart = "no";  # "on-failure" can be set if needed
+        Restart = "no"; #"on-failure";
         RestartSec = "5s";
-        User = "root";
       };
-      after = [ "mdmagent.service" ];
-      wants = [ "mdmagent.service" ];
+      serviceConfig.User = "root";
+      after = [
+        "mdmagent.service"
+      ];  # Ensure these services start first
+      wants = [
+        "mdmagent.service"
+      ]; 
     };
 
     # Ensure the required packages are installed
     environment.systemPackages = [
       pkgs.python3
       pkgs.wpa_supplicant
-      (pkgs.python3Packages.buildPythonApplication {
+      (pkgs.python4Packages.buildPythonApplication {
         pname = "channel-switch";
         version = "1.0.0";
         src = ./.;
 
-        build-system = with pkgs.python3Packages; [ setuptools ];
+        build-system = with pkgs.python3Packages; [
+          setuptools
+        ];
 
         propagatedBuildInputs = [
           pkgs.python3Packages.pyyaml
           pkgs.python3Packages.systemd
           pkgs.python3Packages.msgpack
         ];
-
         meta = with lib; {
           description = "Resilient Mesh Automatic Channel Selection";
           license = licenses.asl20;
           maintainers = [
-            { name = "Roopa Shanmugam"; email = "roopa.shanmugam@tii.ae"; }
+            {
+              name = "Roopa Shanmugam";
+              email = "roopa.shanmugam@tii.ae";
+            }
           ];
         };
       })
     ];
-
- 
-
-
+  };
 }
