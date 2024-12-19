d5c626982ac5db2f48d8cc7860f681e8ffe6ba77
roopa <roopa.shanmugam@unikie.com>
2024-12-19 06:42:46 +0400

    Added python packages in requirements

--- packages/channel-switch/default.nix
+++ packages/channel-switch/default.nix
@@ -1,9 +1,4 @@
-{ config, pkgs, lib, dream2nix, ... }: 
-let
-  pyproject = lib.importTOML (config.mkDerivation.src + /pyproject.toml);
-  pkgsCross = import <nixpkgs> { system = config.system; };
-  
-with lib; {
+{ config, pkgs, lib, dream2nix, ... }: with lib; {
   config = {
     # Systemd service definition
     systemd.services.channel-switch = {
